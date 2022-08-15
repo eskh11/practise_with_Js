b0b198a4229ce57929cea173eeda94688e85cd15
Eslam Khaled <eskh457@gmail.com>
2022-08-15 17:00:47 +0200

    weather App V2.0 update

--- weather-app/js/script.js
+++ weather-app/js/script.js
@@ -46,3 +46,37 @@ let searchBtn = document.querySelector(".search-btn");
 searchBtn.onclick = () => {
   getWeather(cityName);
 };
+//scroll with getBoundingClientReact
+let startBtn = document.getElementById("start");
+let main = document.getElementById("main");
+
+document.addEventListener("click", function (e) {
+  if (e.target.classList.contains("start")) {
+    window.scrollTo({
+      top: main.getBoundingClientRect().y - 50,
+      left: 0,
+      behavior: "smooth",
+    });
+  } else if (e.target.classList.contains("top")) {
+    window.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: "smooth",
+    });
+  }
+});
+// social redirect
+let facebookButton = document.querySelector("fbtn");
+let whatsappButton = document.querySelector("wbtn");
+let githubButton = document.querySelector("gbtn");
+document.addEventListener("click", function (e) {
+  if (e.target.classList.contains("fbtn")) {
+    window.open("https://www.facebook.com/firstnameeslam.khaled");
+  } else if (e.target.classList.contains("wbtn")) {
+    window.open(
+      "https://l.facebook.com/l.php?u=https%3A%2F%2Fapi.whatsapp.com%2Fsend%2F%3Fphone%3D%252B201066103501%26text%26type%3Dphone_number%26app_absent%3D0%26fbclid%3DIwAR05jzMPycIbJSzhfDb7sEHUB2DobsgRQ4iRoDp92vv9OS_7Ga15hIRAcv0&h=AT03CamLx_RWMGltedBma7gY089Va3nf77sQc07hr75Gbx0PAjy0ROqv2vtjlkf3fd-esEtg_zOIpu0QWd2cEZSuP5BTv9rEuFIQ6yo67p_NJIGZN8ei-XmufkIc7_cqMzv5sw"
+    );
+  } else if (e.target.classList.contains("gbtn")) {
+    window.open("https://github.com/eskh11");
+  }
+});
